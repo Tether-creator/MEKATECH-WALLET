31d8ae2be15afe3a7ba701a14a0ef5e1771ce570
Tether-creator <gruthzillions@gmail.com>
2025-02-20 18:27:15 +0100

    Update server.js

--- server.js
+++ server.js
@@ -1,7 +1,23 @@
 const express = require('express');
 const path = require('path');
+const helmet = require('helmet');
 
 const app = express();
+
+// Set Content Security Policy (CSP)
+app.use(
+    helmet({
+        contentSecurityPolicy: {
+            directives: {
+                defaultSrc: ["'self'"],
+                scriptSrc: ["'self'", "'unsafe-inline'", "'unsafe-eval'"],
+                styleSrc: ["'self'", "'unsafe-inline'"],
+                imgSrc: ["'self'", "data:"],
+                connectSrc: ["'self'"],
+            },
+        },
+    })
+);
 
 // Serve static files from the public folder
 app.use(express.static(path.join(__dirname, 'public')));
