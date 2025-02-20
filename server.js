72b88491c0ed215a8e1045ca5e49ee971633e77a
Tether-creator <gruthzillions@gmail.com>
2025-02-20 18:47:11 +0100

    Update server.js

--- server.js
+++ server.js
@@ -40,5 +40,5 @@ app.get('/price', (req, res) => {
 // Start the server
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
-    console.log(Server running on port ${PORT});
+    console.log(`Server running on port ${PORT}`);
 });
