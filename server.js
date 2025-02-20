dfdfa77061a563c1c66663f135cb87598af2287f
Tether-creator <gruthzillions@gmail.com>
2025-02-20 17:34:06 +0100

    Create server.js

---
+++ server.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const cors = require('cors');
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.use(cors());
+app.use(express.static('public'));
+
+app.get('/balance', (req, res) => {
+    res.json({ balance: "100.00 USDT", price: "$1.00" });
+});
+
+app.listen(PORT, () => {
+    console.log(Server running on port ${PORT});
+});
