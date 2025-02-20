6ce569c439d5a2a75e07aada7457374a8d5aba06
Tether-creator <gruthzillions@gmail.com>
2025-02-20 20:51:32 +0100

    Update server.cjs

--- server.cjs
+++ server.cjs
@@ -1,15 +1,17 @@
 const express = require("express");
 const cors = require("cors");
+
 const app = express();
+const PORT = process.env.PORT || 3000;
 
 app.use(cors());
-app.use(express.static("public"));
+app.use(express.json());
 
+// Test Route
 app.get("/", (req, res) => {
-  res.sendFile(__dirname + "/public/index.html");
+  res.send("MEKA-TECH Wallet Backend is Running!");
 });
 
-const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
