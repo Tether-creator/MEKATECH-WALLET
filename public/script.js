8f6ea7371462b0fad817607644fbbe9ff4cfc812
Tether-creator <gruthzillions@gmail.com>
2025-02-21 00:35:27 +0100

    Update script.js

--- public/script.js
+++ public/script.js
@@ -1,6 +1,8 @@
 document.addEventListener("DOMContentLoaded", async function () {
     try {
         const response = await fetch("https://mekatech-wallet.onrender.com/balance");
+        if (!response.ok) throw new Error("Network response was not OK");
+        
         const data = await response.json();
         document.getElementById("balance").innerText = data.balance || "Error fetching balance";
     } catch (error) {
