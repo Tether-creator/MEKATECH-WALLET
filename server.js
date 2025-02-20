6ef0eef4bf4eb05bc14f1a0084ffc95ec0c76dd7
Tether-creator <gruthzillions@gmail.com>
2025-02-20 18:19:38 +0100

    Update server.js

--- server.js
+++ server.js
@@ -11,6 +11,16 @@ app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, 'public', 'index.html'));
 });
 
+// API to return fake balance
+app.get('/balance', (req, res) => {
+    res.json({ balance: 100.00 });
+});
+
+// API to return fake price
+app.get('/price', (req, res) => {
+    res.json({ price: 1.00 });
+});
+
 // Start the server
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
